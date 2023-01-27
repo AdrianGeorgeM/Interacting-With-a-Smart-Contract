ce503dc4aa207031272de5ae438f7000707b9c8b
Adriano <62570164+AdrianGeorgeM@users.noreply.github.com>
2023-01-27 22:34:15 +0000

    Created new ether.Contract

--- index.js
+++ index.js
@@ -1,4 +1,19 @@
+import { contractABI } from "./utils";
 require("dotenv").config();
 const ethers = require("ethers");
 
-function main() {}
+// new ethers.providers.AlchemyProvider([(network = "homestead"), [apiKey]]);
+
+const provider = new ethers.providers.AlchemyProvider(
+	"goerli",
+	process.env.TEST_API_KEY
+);
+
+function main() {
+	// new ethers.Contract(address, abi, signerOrProvider);
+	const counterContract = new ethers.Contract(
+		"0x5F91eCd82b662D645b15Fd7D2e20E5e5701CCB7A",
+		contractABI,
+		provider
+	);
+}
